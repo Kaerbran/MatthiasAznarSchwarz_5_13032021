6f142bba59b4655f6f39c39f97f07583a7d46315
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2021-03-20 19:44:50 +0100

    Chargement de la page HTML de manière dynamique terminée

---
+++ pageFilling.js
@@ -0,0 +1,103 @@
+
+class purchasableObject {
+    constructor(ObjectID, name, price, description, imageURL, productType){
+        this.ObjectID = ObjectID;
+        this.name = name;
+        this.price = price;
+        this.description = description;
+        this.imageURL = imageURL;
+        this.productType = productType;
+        this.personnalisation = personnalisation;
+    }
+}
+
+class contactDetails {
+    constructor(firstName, lastName, address, city, mail){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.address = address;
+        this.city = city;
+        this.mail = mail;
+    }
+}
+
+//fonction GetAllTeddy : nous permet de recevoir un Array avec l'ensemble des ours
+const getAllTeddy = async function () {
+    try{
+        let responseAllTeddy = await fetch('http://localhost:3000/api/teddies/')
+        if(responseAllTeddy.ok){
+            let data = await responseAllTeddy.json()
+            console.log(data)
+            return data;
+        } else{
+            console.error('Error code from server : ${error}');
+        }
+    } catch (e) {
+        console.log(e);
+    }
+}
+
+getAllTeddy()
+.then(function (objectTeddy) {
+    let anchorMainContent = document.getElementById('mainMain');
+    for (let index = 0; index < objectTeddy.length; index++) {
+        //Ci-dessous l'article est crée et lié à la balise ''main''
+        const newArticleElem = document.createElement("article");
+        newArticleElem.classList.add("mainCard");
+        newArticleElem.id = objectTeddy[index]._id;
+        anchorMainContent.appendChild(newArticleElem);
+
+        //Ci-dessous le chemin DOM vers l'element qui vient d'être creer
+        let anchorNewContent = document.getElementById(objectTeddy[index]._id);
+
+        //Ci-dessous l'image est crée et lié à l'article crée
+        const newImageElem = document.createElement("img");
+        newImageElem.classList.add("mainCard__img");
+        newImageElem.src = objectTeddy[index].imageUrl;
+        newImageElem.alt = 'Un beau nounours!';
+        anchorNewContent.appendChild(newImageElem);
+
+        //Ci-dessous div class="mainCard__txtdiv" est crée et lié à l'article crée
+        const newDivElem = document.createElement("div");
+        newDivElem.classList.add("mainCard__txtdiv");
+        anchorNewContent.appendChild(newDivElem);
+
+        //Ci-dessous je crée une ancre pour savoir ou placer le contenu h2 et p à l'interieur de la carte
+        let anchorArrayNewTextContainer = document.getElementsByClassName("mainCard__txtdiv");
+        let anchorNewTextContainer = anchorArrayNewTextContainer[index];
+
+            //Ci-dessous le h2 est crée et lié à la div qui vient d'être crée
+            const newH2Elem = document.createElement("h2");
+            newH2Elem.classList.add("mainCard__txtdiv__h2");
+            newH2Elem.innerHTML = objectTeddy[index].name;
+            anchorNewTextContainer.appendChild(newH2Elem);
+
+            //Ci-dessous le p est crée et lié à la div qui vient d'être crée
+            const newPElem = document.createElement("p");
+            newPElem.classList.add("mainCard__txtdiv__p");
+            newPElem.innerHTML = objectTeddy[index].description;
+            anchorNewTextContainer.appendChild(newPElem);
+            //objectTeddy[index].description
+
+        //Ci-dessous button est crée et lié à l'article crée
+        const newBttElem = document.createElement("button");
+        newBttElem.classList.add("btn");
+        newBttElem.classList.add("btn--blue");
+        newBttElem.classList.add("mainCard__btt");
+        newBttElem.innerHTML = "Ajouter au panier";
+        anchorNewContent.appendChild(newBttElem);
+
+        /*
+        <article class="mainCard">
+            <img class="mainCard__img" src="./public/css/img/teddy_1.jpg" alt="Peluche d'ourse brun, fait main.">
+            <div class="mainCard__txtdiv">
+                <h2 class="mainCard__txtdiv__h2">Nounours brun</h2>
+                <p class="mainCard__txtdiv__p">Un beau nounours brun, fabriqué main, par un artisan Ardechois. L'atlier a été fondé par l'arrière grand-père, un grand passioné de peluches.</p>
+            </div>
+            <button class="btn btn--blue mainCard__btt">Ajouter au panier</button>
+        </article>
+        */
+        
+    }
+    console.log(objectTeddy[1]);
+});
