42daa514f70eb378fc1606a703023549ee7eb4b2
Matthias Aznar-Schwarz <m.aznar.schwarz@gmail.com>
2021-04-18 17:49:39 +0200

    SessionStorage is now properly storing the basket data

--- pageFillingArticle.js
+++ pageFillingArticle.js
@@ -41,26 +41,59 @@ getTeddy(GetURL)
 
 //Cette fonction permet lors d'un click, d'enregistrer les Teddy ajoutés au panier
 const AddToBasket = function () {
-    
     if (document.getElementById('articleOptionsTeddy').options[document.getElementById('articleOptionsTeddy').selectedIndex].value != "no_selection") {
         let Article = {
             name : document.getElementById('articleNameTeddy').innerHTML,
             id : document.getElementById('articleNameTeddy').titel,
             color : document.getElementById('articleOptionsTeddy').options[document.getElementById('articleOptionsTeddy').selectedIndex].value
         };
+        console.log('%c Find below the object that needs to be added to the basket', 'color: green; font-weight: bold;');
         console.log(Article);
 
+        //fonction permettant d'enregistrer les articles que l'on met dans le panier
+        AddToDataStorage (Article, 'listeBasketTest3', true);
+
+        let TestStoredData = ExtractFromDataStorage ('listeBasketTest3');
+        console.log('%c Ci-dessous les données qui viennent juste d être sauvegardées.', 'color: green; font-weight: bold;');
+        console.log(TestStoredData);
+
     } else{
-        console.log("Requête non sauvegardé, car aucune sélection n'a été faite.")
+        console.log('%c Requête non sauvegardé, car aucune sélection n a été faite.', 'color: red; font-weight: bold;');
     }
-
-    //il manque ici la mise en memoire de l'object dans un tableau... 
-
 }
 
-//Cette fonction me permet de stocker des objects dans la session Storage. Sous la forme d'un tableau. 
-const AddToDataStorage = function (ObjectToStore, WhereToStore) {
+//Cette fonction permet de stocker (des objects) dans la session Storage. (Sous la forme d'un tableau.)
+const AddToDataStorage = function (ObjectToStore, StorageLocationName, AddToMemory) {
+    if (typeof Storage !== "undefined") {
+        // localStorage & sessionStorage support!
 
-    sessionStorage.setItem(WhereToStore, JSON.stringify(ObjectToStore));
+        if (AddToMemory && sessionStorage.getItem(StorageLocationName) != null){
+            let allreadyStoredObject = ExtractFromDataStorage(StorageLocationName);
+            //console.log('%c Find below the list of all teddies that are in the previous shopping list', 'color: orange; font-weight: bold;');
+            //console.log (allreadyStoredObject);
+            
+            allreadyStoredObject.push(ObjectToStore);
+            //console.log('%c Find below the list of all teddies that are in the new shopping list', 'color: orange; font-weight: bold;');
+            //console.log (allreadyStoredObject);
 
+            sessionStorage.setItem(StorageLocationName, JSON.stringify(allreadyStoredObject));
+        }
+        else{
+            let ArrayToStore = [];
+            ArrayToStore.push(ObjectToStore);
+            sessionStorage.setItem(StorageLocationName, JSON.stringify(ArrayToStore));
+            //console.log('%c Find below the list of the teddy that was added in the shopping list', 'color: green; font-weight: bold;');
+            //console.log (ArrayToStore);
+        }
+    }
+    else {
+        // No web storage support
+        console.log('%c No web storage support', 'color: red; font-weight: bold;');
+    }
+}
+
+//Cette fonction permet d'extraire des variables stockées
+const ExtractFromDataStorage = function (StorageLocationName){
+    var obj = JSON.parse(sessionStorage.[StorageLocationName]);
+    return obj;
 }
